b0a79eacdad5cf08b2000c289bfb548184420937
wzfeilongge <494379797@qq.com>
2019-12-25 11:00:35 +0800

    222

---
+++ Element.Domain/Events/UserEvent/ChangePwdEvent.cs
@@ -0,0 +1,31 @@
+﻿using Element.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element.Domain.Events.UserEvent
+{
+  public  class ChangePwdEvent:Event
+    {
+
+        public string Name { get; private set; }
+
+        public string Email { get;  private set; }
+
+        public string Ip { get; private set; }
+
+        public ChangePwdEvent(Guid Id,string Name,string Email, string Ip)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.AggregateId = Id;
+            this.Ip = Ip;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Element.Domain/Events/UserEvent/RegisterUserEvent.cs
@@ -0,0 +1,31 @@
+﻿using Element.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element.Domain.Events.UserEvent
+{
+    public class RegisterUserEvent : Event
+    {
+
+        public RegisterUserEvent(Guid Id,string IdCard, string Phone, string Address, string Name)
+        {
+            this.IdCard = IdCard;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.Name = Name;
+            this.AggregateId = Id;
+        }
+
+        public string IdCard { get; private set; }
+
+        public string Phone { get; private set; }
+
+        public string Address { get; private set; }
+
+        public string Name { get; private set; }
+
+
+
+    }
+}
